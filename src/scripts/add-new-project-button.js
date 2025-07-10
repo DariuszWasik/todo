ed393bafd08c1d1f332c00da55987f2abc7d8462
Dario_W <wasik.dariusz24@gmail.com>
2025-07-10 22:57:17 +0200

    dialog add-project styling plus fixing loading dialog on start

--- src/scripts/add-new-project-button.js
+++ src/scripts/add-new-project-button.js
@@ -1,10 +1,11 @@
-
+import { createAddProjectModal } from "./modals/DOMcreateProjectsDialog";
 
 export function addProject(){
-const addProjectBtn = document.querySelector('.add-project');
-const addProjectDialog = document.querySelector('.add-project-dialog')
-addProjectBtn.addEventListener( 'click', () => {
-   addProjectDialog.showModal()
+    const addProjectBtn = document.querySelector('.add-project');
+    const addProjectDialog = document.querySelector('.add-project-dialog')
+    addProjectBtn.addEventListener( 'click', () => {
+    addProjectDialog.showModal()
+    createAddProjectModal();
 })
 }
 
---
+++ src/scripts/modals/DOMcreateProjectsDialog.js
@@ -0,0 +1,45 @@
+import './styles/addProjectModal.css'
+
+
+
+export function createAddProjectModal() {
+    const addProjectModal = document.querySelector('.add-project-dialog');
+    
+    const title = document.createElement('p');
+    title.textContent = 'New Project';
+    
+    const nameLabel = document.createElement('label');
+    nameLabel.className = 'nameLabel';
+    nameLabel.textContent = 'name:'
+    const nameInput = document.createElement('input');
+    nameInput.className = 'nameInput';
+    
+    const nameDiv = document.createElement('div');
+    nameDiv.className = 'nameDiv';
+    nameDiv.append(nameLabel, nameInput)
+    
+    const descLabel = document.createElement('label');
+    descLabel.className = 'descLabel';
+    descLabel.textContent = 'description:'
+    const descInput = document.createElement('textarea');
+    descInput.className = 'descInput';
+    
+    const descDiv = document.createElement('div');
+    descDiv.className = 'descDiv';
+    descDiv.append(descLabel, descInput)
+    
+    
+    const addBtn = document.createElement('button');
+    addBtn.className = 'addBtn';
+    addBtn.innerText = 'Add'
+    const cancelBtn = document.createElement('button');
+    cancelBtn.className = 'cancelBtn';
+    cancelBtn.innerText = 'Cancel'
+    
+    const btnDiv = document.createElement('div');
+    btnDiv.className = 'btnDiv';
+    btnDiv.append(addBtn, cancelBtn)
+    
+    
+    addProjectModal.append(title, nameDiv, descDiv, btnDiv);
+}
