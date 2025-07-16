deaabf38b73f134c98801f89089ec5830967b7d5
Dario_W <wasik.dariusz24@gmail.com>
2025-07-16 15:10:34 +0200

    make addTask in addTodo dialog working and adding todo to proper project

--- src/scripts/modals/addTodoFunc.js
+++ src/scripts/modals/addTodoFunc.js
@@ -1,5 +1,5 @@
 import { ToDo } from "../todo";
-
+import { projectsArr } from "../project";
 
    
 export { cancelNewTodo, addNewTodo }
@@ -22,9 +22,13 @@ export { cancelNewTodo, addNewTodo }
         const descInput = document.querySelector('.descInput');
         const dateInput = document.querySelector('.dateInput');
         const selectInput = document.querySelector('#selectProjectInput');
+        const priorityInput = document.querySelector('#priorityInput')
 
         addTodoBtn.addEventListener('click', () => {
-        const newTodo = new ToDo (nameInput.value, descInput.value, dateInput.value, selectInput.value, 2 );
+        const projectToGo = projectsArr.filter(a => a.name == selectInput.value);
+        const newTodo = new ToDo (nameInput.value, descInput.value, dateInput.value, priorityInput.value, projectToGo[0].id);
         console.log(newTodo)
+        newTodo.assignToProject();
+        closeTodoDialog();
         })
     }
