037fabc57e4d0456807d944d37ea4c3090b4599a
Dario_W <wasik.dariusz24@gmail.com>
2025-07-17 23:25:04 +0200

    instal npm library date-fns and format dates of projects

--- src/scripts/init.js
+++ src/scripts/init.js
@@ -5,8 +5,8 @@ import { addProject } from "./add-new-project-button";
 import { displayAllTodos } from "./DOM-todoCard";
 import { projectsButtonDisplayingItsTodos } from "./projectBtnDisplayTodos";
 import { addTask } from "./addToDoBtn";
-
-
+import { format } from "date-fns";
+import { add } from "date-fns";
 
 export function init () {
     console.log('siema')
@@ -20,15 +20,15 @@ telefony.putProjectInProjectsArr();
 console.log(zadania, telefony);
 console.log(projectsArr);
 
-const zadzwonDoLekarza = new ToDo('zadzown do lekarz', 'zapytaj o recepte Ani', '1/2/2024', 'high', 2);
+const zadzwonDoLekarza = new ToDo('zadzown do lekarz', 'zapytaj o recepte Ani', format(add(new Date(), {days: -3}), 'dd MM yyyy'), 'high', 2);
 console.log(zadzwonDoLekarza)
 zadzwonDoLekarza.assignToProject();
 
-const teleankieta = new ToDo('zadzown energetyki', 'wez udzial w ankiecie', '1/4/2024', 'high', 2);
+const teleankieta = new ToDo('zadzown energetyki', 'wez udzial w ankiecie', format(new Date(), 'eee  dd-MM-yyyy'), 'high', 2);
 console.log(teleankieta);
 teleankieta.assignToProject();
 
-const rozliczPodatek = new ToDo('rozlicz podatek', 'sprobuj uzyskac ulge na dzieci', '25/1/2025', 'low', 0);
+const rozliczPodatek = new ToDo('rozlicz podatek', 'sprobuj uzyskac ulge na dzieci',format(add(new Date(), {days: 6}), 'eee dd MMM yyyy'), 'low', 0);
 rozliczPodatek.assignToProject();
  loadActions();
 }
