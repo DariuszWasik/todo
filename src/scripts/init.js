5642347c6b586e50cc39a2e76290a4c105f649ce
Dario_W <wasik.dariusz24@gmail.com>
2025-07-22 14:31:13 +0200

    make navigation button working

--- src/scripts/init.js
+++ src/scripts/init.js
@@ -7,6 +7,7 @@ import { projectsButtonDisplayingItsTodos } from "./projectBtnDisplayTodos";
 import { addTask } from "./addToDoBtn";
 import { format } from "date-fns";
 import { add } from "date-fns";
+import { giveNavigationButtonsFunc } from "./navigationButtons";
 
 export function init () {
     console.log('siema')
@@ -20,15 +21,15 @@ telefony.putProjectInProjectsArr();
 console.log(zadania, telefony);
 console.log(projectsArr);
 
-const zadzwonDoLekarza = new ToDo('zadzown do lekarz', 'zapytaj o recepte Ani', format(add(new Date(), {days: -3}), 'dd MM yyyy'), 'high', 2);
+const zadzwonDoLekarza = new ToDo('zadzown do lekarz', 'zapytaj o recepte Ani', format(add(new Date(), {days: -3}), 'eee, dd MMM yyy'), 'high', 2);
 console.log(zadzwonDoLekarza)
 zadzwonDoLekarza.assignToProject();
 
-const teleankieta = new ToDo('zadzown energetyki', 'wez udzial w ankiecie', format(new Date(), 'eee  dd-MM-yyyy'), 'high', 2);
+const teleankieta = new ToDo('zadzown energetyki', 'wez udzial w ankiecie', format(new Date(), 'eee, dd MMM yyy'), 'high', 2);
 console.log(teleankieta);
 teleankieta.assignToProject();
 
-const rozliczPodatek = new ToDo('rozlicz podatek', 'sprobuj uzyskac ulge na dzieci',format(add(new Date(), {days: 6}), 'eee dd MMM'), 'low', 0);
+const rozliczPodatek = new ToDo('rozlicz podatek', 'sprobuj uzyskac ulge na dzieci',format(add(new Date(), {days: 6}), 'eee, dd MMM yyy'), 'low', 0);
 rozliczPodatek.assignToProject();
  loadActions();
 }
@@ -38,5 +39,6 @@ function loadActions() {
     addProject();
     addTask();   
     displayAllTodos();
-    projectsButtonDisplayingItsTodos()
+    projectsButtonDisplayingItsTodos();
+    giveNavigationButtonsFunc();
 }
---
+++ src/scripts/navigationButtons.js
@@ -0,0 +1,70 @@
+import { allTodosArr } from "./todo";
+import { createTodoCard, container } from "./DOM-todoCard";
+import { format } from "date-fns";
+import { add } from "date-fns";
+import { isBefore } from "date-fns";
+import { isAfter } from "date-fns";
+
+const allBtn = document.querySelector('.all')
+const todayBtn = document.querySelector('.today')
+const importantBtn = document.querySelector('.important')
+const weekBtn = document.querySelector('.this-week')
+const overdueBtn = document.querySelector('.overdue')
+export function giveNavigationButtonsFunc(){
+    console.log(allTodosArr, 'from naviButton')
+      allBtn.addEventListener('click', displayAllByDates);
+      todayBtn.addEventListener('click', displayTodayTasks);
+      importantBtn.addEventListener('click', displayImportant)
+      weekBtn.addEventListener('click', displayWeek)
+        overdueBtn.addEventListener('click', displayOverdue)
+        }
+
+
+
+function displayAllByDates() {
+    // make a copy so the orginal allTodosArr stays unchanged
+    const copy = allTodosArr.slice()
+    copy.sort((a, b) => new Date(a.dueDate) - new Date(b.dueDate))
+    console.log(allTodosArr, 'posortowane allTodosArr', copy, 'copy posortowane');
+    container.innerHTML =''
+    copy.forEach(el => createTodoCard(el))
+}
+
+
+function displayTodayTasks() {
+    console.log(allTodosArr, format(new Date(), 'eee, dd MMM yyy'))
+    // make a copy so the orginal allTodosArr stays unchanged
+    const copy = allTodosArr.slice();
+    const filtered = copy.filter(a => a.dueDate == format(new Date(), 'eee, dd MMM yyy'));
+    console.log(filtered)
+    container.innerHTML =''
+    filtered.forEach(el => createTodoCard(el))
+}
+
+function displayImportant(){
+    const filtered = allTodosArr.filter(a => a.priority == 'high');
+    container.innerHTML =''
+    filtered.forEach(el => createTodoCard(el))
+}
+
+function displayWeek() {
+    const filtered = []
+    allTodosArr.filter(a => {
+        if ((isBefore(new Date(a.dueDate), add(new Date(), {days: 8}))) && (isAfter(new Date(a.dueDate), add(new Date(), {days: -1})))) {
+            filtered.push(a)
+        }})
+    console.log(allTodosArr)
+    container.innerHTML =''
+    filtered.forEach(el => createTodoCard(el))
+}
+
+function displayOverdue() {
+    const filtered = []
+    allTodosArr.filter(a => {
+        if ((isBefore(new Date(a.dueDate), add(new Date(), {days: -1})))) {
+            filtered.push(a)
+        }})
+    container.innerHTML =''
+    filtered.forEach(el => createTodoCard(el))
+    
+}
