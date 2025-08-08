8276802078d3ba2db7696152b656fece01eebd9c
Dario_W <wasik.dariusz24@gmail.com>
2025-08-08 14:42:13 +0200

    make deleteProject function work poreperly, updating project panel and deleting all todos which belong to deleted project

--- src/scripts/deleteProject.js
+++ src/scripts/deleteProject.js
@@ -1,16 +1,27 @@
 import { projectsArr } from "./project";
-
-
+import { displayProjects } from "./DOM-all-projects-container";
+import { putAllTodosInAllTodosArray } from "./todo";
+import { projectsButtonDisplayingItsTodos } from "./projectBtnDisplayTodos";
+import { displayAllByDates } from "./navigationButtons";
 
 export function deleteProjectFunc() {
 const deleteProjectBtn = document.querySelector('.delete-project');
 deleteProjectBtn.addEventListener('click', () => {
     console.log('click delete id nr', deleteProjectBtn.name)
     const thisProject = projectsArr.filter(a => a.name == deleteProjectBtn.name)
-    const numberInProjectsArr = thisProject[0].id;
+    const numberInProjectsArr = projectsArr.indexOf(thisProject[0]);
+
+
     projectsArr.splice(numberInProjectsArr, 1)
     console.log(thisProject[0],
     numberInProjectsArr,
     projectsArr);
+    
+    displayProjects()
+    projectsButtonDisplayingItsTodos()
+    putAllTodosInAllTodosArray()
+
+
+    displayAllByDates()
 })
 }
