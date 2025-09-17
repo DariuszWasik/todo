f035719af8fcf009f815b27041115f622da30319
Dario_W <wasik.dariusz24@gmail.com>
2025-09-17 21:50:24 +0200

    remove manualy creating new Project and new Todo from init.js and replace it by locoStorage() where everything is created based on JSON with data of same projects and todos manually created before in init

--- src/scripts/init.js
+++ src/scripts/init.js
@@ -20,32 +20,33 @@ export function init () {
 // console.log(projectsArr);
 
 
-const zadania = new Project('zadania', 'do wykonania niebawem');
-zadania.putProjectInProjectsArr();
-const zadania2 = new Project('zadania inne', 'do przemyslenia');
-zadania2.putProjectInProjectsArr();
-const telefony = new Project('telefony', 'wazne kontakty')
-telefony.putProjectInProjectsArr();
-console.log(zadania, telefony);
-console.log(projectsArr);
+// const zadania = new Project('zadania', 'do wykonania niebawem');
+// zadania.putProjectInProjectsArr();
+// const zadania2 = new Project('zadania inne', 'do przemyslenia');
+// zadania2.putProjectInProjectsArr();
+// const telefony = new Project('telefony', 'wazne kontakty')
+// telefony.putProjectInProjectsArr();
+// console.log(zadania, telefony);
+// console.log(projectsArr);
 
-const zadzwonDoLekarza = new ToDo('zadzown do lekarz', 'zapytaj o recepte Ani', format(add(new Date(), {days: -3}), 'eee, dd MMM yyy'), 'high', projectsArr[2].id);
-console.log(zadzwonDoLekarza)
-zadzwonDoLekarza.assignToProject();
+// const zadzwonDoLekarza = new ToDo('zadzown do lekarz', 'zapytaj o recepte Ani', format(add(new Date(), {days: -3}), 'eee, dd MMM yyy'), 'high', projectsArr[2].id);
+// console.log(zadzwonDoLekarza)
+// zadzwonDoLekarza.assignToProject();
 
-const teleankieta = new ToDo('zadzown energetyki', 'wez udzial w ankiecie', format(new Date(), 'eee, dd MMM yyy'), 'high', projectsArr[2].id);
-console.log(teleankieta);
-teleankieta.assignToProject();
+// const teleankieta = new ToDo('zadzown energetyki', 'wez udzial w ankiecie', format(new Date(), 'eee, dd MMM yyy'), 'high', projectsArr[2].id);
+// console.log(teleankieta);
+// teleankieta.assignToProject();
 
-const rozliczPodatek = new ToDo('rozlicz podatek', 'sprobuj uzyskac ulge na dzieci',format(add(new Date(), {days: 6}), 'eee, dd MMM yyy'), 'low', projectsArr[0].id);
-rozliczPodatek.assignToProject();
+// const rozliczPodatek = new ToDo('rozlicz podatek', 'sprobuj uzyskac ulge na dzieci',format(add(new Date(), {days: 6}), 'eee, dd MMM yyy'), 'low', projectsArr[0].id);
+// rozliczPodatek.assignToProject();
  
 
 loadActions();
 }
 
 export function loadActions() {
-    putAllTodosInAllTodosArray()
+    // putAllTodosInAllTodosArray()
+    localStorage()
     displayProjects();
     addProject();
     addTask();   
@@ -54,5 +55,4 @@ export function loadActions() {
     displayAllByDates()
     changeMode()
     smallScreenFunc()
-    localStorage()
 }
--- src/scripts/localStorage.js
+++ src/scripts/localStorage.js
@@ -1,20 +1,24 @@
 import { projectsArr, Project } from "./project";
-import { allTodosArr } from "./todo";
+import { allTodosArr, putAllTodosInAllTodosArray } from "./todo";
 import { ToDo } from "./todo";
-import { putAllTodosInAllTodosArray } from "./todo";
 import { initialJSON } from "./intialJSON";
 
 export function localStorage() {
     console.log(projectsArr, 'projects')
     console.log(allTodosArr, 'todos')
-    console.log(JSON.stringify(projectsArr))
-    const projectsArrJSON = JSON.stringify(projectsArr)
+    // console.log(JSON.stringify(projectsArr))
+    // const projectsArrJSON = JSON.stringify(projectsArr)
     const copy = JSON.parse(initialJSON);
     console.log(copy, 'copy')
-    console.log(typeof(copy))
-    console.log(typeof(projectsArr))
-    console.log(copy[0].todos), 'copy[0]'
-    const copyProjectsArr = []
+    // console.log(typeof(copy))
+    // console.log(typeof(projectsArr))
+    // console.log(copy[0].todos), 'copy[0]'
+    // const copyProjectsArr = []
+    projectsArr.splice(0, projectsArr.length)
+    // projectsArr.length = 0;
+    console.log(projectsArr, 'after')
+    console.log(projectsArr[1])
+
     copy.forEach(element => { 
         const project =  new Project(element.name, element.description);
         project.id = element.id;
@@ -24,19 +28,19 @@ export function localStorage() {
             console.log(todo, 'todo')
             project.todos.push(todo)
         })
-        copyProjectsArr.push(project)
+        projectsArr.push(project)
     });
     console.log(projectsArr, 'projects')
-    console.log(copyProjectsArr, 'copyProjectsArr')
-    console.log( copyProjectsArr === projectsArr)
+    // console.log(copyProjectsArr, 'copyProjectsArr')
+    // console.log( copyProjectsArr === projectsArr)
 
-    let copyAllTodosArr = [];
-        copyProjectsArr.map(a => a.todos).forEach(el => 
-        {el.forEach(a => copyAllTodosArr.push(a))})
+    // let copyAllTodosArr = [];
     
-    console.log(copyAllTodosArr, 'copyAlltodosArr')
+    putAllTodosInAllTodosArray();
+
+    // console.log(copyAllTodosArr, 'copyAlltodosArr')
     console.log(allTodosArr, 'alltodosarr')
-    console.log(allTodosArr == copyAllTodosArr)
+    // console.log(allTodosArr == copyAllTodosArr)
     
 
 }
